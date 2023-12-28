41a9debab785e0a86494a5fb053c856d520b972f
xiaoy <1103098607@qq.com>
2023-12-28 11:40:40 +0800

    feat: add util config and go.mod

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/soft-feather/TinyReading
+
+go 1.19
+
+require github.com/wonderivan/logger v1.0.0
+
+require gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ util/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import "gopkg.in/ini.v1"
+
+var DefaultCfg = &Config{}
+var defaultConfigPath = "./config.ini"
+
+// TODO 设置各种选项默认值
+
+func LoadDefaultConfig(path string) error {
+	var (
+		pf  *ini.File
+		err error
+	)
+	pf, err = ini.Load(path)
+	if err != nil {
+		return err
+	}
+	err = pf.MapTo(DefaultCfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type Default struct {
+	AddrLs []string `ini:"addr_ls"`
+}
+
+type Config struct {
+	Default `ini:"default"`
+}
+
+// Load 只会在启动时被调用
+func Load() error {
+	err := LoadDefaultConfig(defaultConfigPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetConfig 获取配置使用这个方法
+func GetConfig() Config {
+	return *DefaultCfg
+}
