a3168e5d6ca542a219d62409851a39a96111c82d
xiaoy <1103098607@qq.com>
2024-01-04 13:29:26 +0800

    feat: book view handle implement with test file

--- module/webserver/api/view/handle.go
+++ module/webserver/api/view/handle.go
@@ -1,6 +1,11 @@
 package view
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/soft-feather/TinyReading/util/common/profile"
+	"github.com/wonderivan/logger"
+	"path"
+)
 
 // BookViewHandler 获取书籍视图，返回相应的书籍文件流
 func BookViewHandler(ctx *gin.Context) {
@@ -9,5 +14,7 @@ func BookViewHandler(ctx *gin.Context) {
 	// 根据 id 查询
 	// 获取书籍 绝对地址
 	// 返回书籍文件
-	ctx.File("")
+	testFile := path.Join(profile.GetEtcProfile().CWD, "doc/pdf", "test.pdf")
+	logger.Debug("test file: ", testFile)
+	ctx.File(testFile)
 }
